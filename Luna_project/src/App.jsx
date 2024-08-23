000b8acd78f336b67a7950ebec7994d5ca22db8c
kwrona05 <kwrona05@gmail.com>
2024-08-23 22:19:45 +0200

    plan

--- Luna_project/src/App.jsx
+++ Luna_project/src/App.jsx
@@ -1,4 +1,11 @@
 function App() {
 
+//import useState aby przechowywać dane
+//import useEffect aby wysłać zapytanie do endpointu GET
+//ul lista
+//każdy element listy ma mieć: name, available, targetTemperature
+//Wyświetlanie ewentualnych błędów
+//moduły tworzyć w obiektach
+
 }
 export default App
