bb448a84a90d7f308c9242f120247ee3fe961b76
kwrona05 <kwrona05@gmail.com>
2024-08-25 08:25:57 +0200

    Create component with module details

---
+++ Luna_project/src/ModuleDetails.jsx
@@ -0,0 +1,15 @@
+function ModuleDetails() {
+    
+    const { id } = useParams();
+    const [module, setModule] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchModuleDetails = async () => {
+
+        }
+    })
+    return(
+
+    );
+};
