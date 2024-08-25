77c98e9c9a60357f3e3e3b90ac52168472c00fb2
kwrona05 <kwrona05@gmail.com>
2024-08-25 08:25:43 +0200

    Add routing to App

--- Luna_project/src/App.jsx
+++ Luna_project/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
 
 function App() {
     const [modules, setModules] = useState([]);
@@ -6,7 +7,7 @@ function App() {
     useEffect(() => {
         const fetchModules = async () => {
             try{
-                const response = await fetch('/modules');
+                const response = await fetch('http://localhost:3001/modules');
                 const data = await response.json();
                 
                 setModules(data);
@@ -19,18 +20,32 @@ function App() {
     }, []);
 
     return(
-        <div className="container">
+        <Router>
+            <div className="container">
             <h1>Modules list</h1>
-            <ul>
+            <Routes>
+            <Route path="/" element={<Module_list modules={modules} />} />
+            <Route path="/modules/:id" element={<ModuleDetails />} />
+            </Routes>
+            </div>
+        </Router>
+
+    );
+};
+
+const Module_list = ( {modules} ) => {
+    return(
+        <ul>
                 {modules.map((module) => (
                     <li key={module.id}>
                         <h2>{module.name}</h2>
                         <p className="available">Available: {module.available ? 'Available' : 'Unavailable'}</p>
                         <p className="temperature">Target Temperature: {module.targetTemperature} °C</p>
+                        <Link to={`/modules/${module.id}`}></Link>
                     </li>
                 ))}
             </ul>
-        </div>
     );
 };
+
 export default App;
