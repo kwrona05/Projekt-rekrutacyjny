dca169580695cb23688c339b93a8e1a665d41be8
kwrona05 <kwrona05@gmail.com>
2024-08-25 17:11:37 +0200

    remove React import

--- Luna_project/src/App.jsx
+++ Luna_project/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ModuleDetails from "./ModuleDetails";
 import ModulesList from "./ModulesList";
