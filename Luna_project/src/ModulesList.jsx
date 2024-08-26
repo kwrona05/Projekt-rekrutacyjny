871862f653daa4213a9a55dce1c27b22119061f5
kwrona05 <kwrona05@gmail.com>
2024-08-26 10:36:58 +0200

    update ModulesList

--- Luna_project/src/ModulesList.jsx
+++ Luna_project/src/ModulesList.jsx
@@ -1,21 +1,25 @@
-import PropTypes from "prop-types"
-import { Link } from 'react-router-dom';
-import {UlStyled} from './ModuleList.styled';
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { UlStyled } from "./ModuleList.styled";
 
-const ModulesList = ( {modules} ) => {
-    return(
-        <UlStyled>
-                {modules.map((module) => (
-                    <li key={module.id}>
-                        <h2>{module.name}</h2>
-                        <p className="available">Available: {module.available ? 'Available' : 'Unavailable'}</p>
-                        <p className="temperature">Target Temperature: {module.targetTemperature} °C</p>
-                        <Link to={`/modules/${module.id}`}>Show details</Link>
-                    </li>
-                ))}
-            </UlStyled>
-    );
+const ModulesList = ({ modules }) => {
+  return (
+    <UlStyled>
+      {modules.map((module) => (
+        <li key={module.id}>
+          <h2>{module.name}</h2>
+          <p className="available">
+            Available: {module.available ? "Available" : "Unavailable"}
+          </p>
+          <p className="temperature">
+            Target Temperature: {module.targetTemperature} °C
+          </p>
+          <Link to={`/modules/${module.id}`}>Show details</Link>
+        </li>
+      ))}
+    </UlStyled>
+  );
 };
 
-ModulesList.propTypes = {modules: PropTypes.array}
+ModulesList.propTypes = { modules: PropTypes.array };
 export default ModulesList;
