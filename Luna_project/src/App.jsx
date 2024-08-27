6c56fad1a47f0cc16881b576336d451ddd489d4a
kwrona05 <kwrona05@gmail.com>
2024-08-27 09:30:10 +0200

    import App.css

--- Luna_project/src/App.jsx
+++ Luna_project/src/App.jsx
@@ -2,36 +2,36 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ModuleDetails from "./ModuleDetails";
 import ModulesList from "./ModulesList";
+import "./App.css";
 
 function App() {
-    const [modules, setModules] = useState([]);
+  const [modules, setModules] = useState([]);
 
-    useEffect(() => {
-        const fetchModules = async () => {
-            try{
-                const response = await fetch('http://localhost:3001/modules');
-                const data = await response.json();
-                
-                setModules(data);
-            } catch (error) {
-                console.error('Module download error', error);
-            }
-        };
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/modules");
+        const data = await response.json();
 
-        fetchModules();
-    }, []);
+        setModules(data);
+      } catch (error) {
+        console.error("Module download error", error);
+      }
+    };
 
-    return(
-        <Router>
-            <div className="container">
-            <h1>Modules list</h1>
-            <Routes>
-            <Route path="/modules/:id" element={<ModuleDetails />} />
-            <Route path="/" element={<ModulesList modules={modules} />} />
-            </Routes>
-            </div>
-        </Router>
+    fetchModules();
+  }, []);
 
-    );
-};
+  return (
+    <Router>
+      <div className="container">
+        <h1>Modules list</h1>
+        <Routes>
+          <Route path="/modules/:id" element={<ModuleDetails />} />
+          <Route path="/" element={<ModulesList modules={modules} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 export default App;
