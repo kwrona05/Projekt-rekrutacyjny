c76f389f08e112ea0fd68a926b17a682afa71a81
kwrona05 <kwrona05@gmail.com>
2024-08-28 14:45:37 +0200

    Add moduleName tests

--- Luna_project/src/ModuleList.test.js
+++ Luna_project/src/ModuleList.test.js
@@ -4,33 +4,7 @@ import "@testing-library/jest-dom"
 import { render, screen } from '@testing-library/react'
 import ModuleList from './ModulesList';
 import { MemoryRouter } from "react-router-dom";
-
-const mockModules = [
-  {
-      id: "0a0f77eb1-50a0-4d98-8116-064fc5a84693",
-      name: "Hydroponic module 1",
-      description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget nullam non nisi est sit amet.",
-      available: true,
-      targetTemperature: 10.0,
-  },
-  {
-      id: "4d0aa62c-b1a9-489d-b4a2-fc16b878ba47",
-      name: "Hydroponic module 2",
-      description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla aliquet enim tortor at auctor.",
-      available: false,
-      targetTemperature: 15.5,
-  },
-  {
-      id: "d4928094-8ef8-48be-823a-4cddef643249",
-      name: "Hydroponic module 3",
-      description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Diam in arcu cursus euismod quis viverra nibh cras.",
-      available: true,
-      targetTemperature: 20.0,
-  },
-]
+import { mockModules } from "../__mocks__/mockModules";
 
 // query* functions will return the element or null if it cannot be found
   // get* functions will return the element or throw an error if it cannot be found
@@ -42,9 +16,21 @@ const mockModules = [
 test("testing link elements", () => {
 
   render(<MemoryRouter><ModuleList modules={mockModules} /></MemoryRouter>)
+
     const links = screen.getAllByTestId('modules-list')
-    
+
     expect(links).not.toBeUndefined()
     expect(links.length).toBe(3)
 }
 )
+
+test("testing module name", () => {
+  render(<MemoryRouter><ModuleList modules={mockModules}></ModuleList></MemoryRouter>)
+
+  const moduleNames = screen.getAllByTestId('module-name')
+
+  expect(moduleNames).toHaveLength(3)
+  expect(moduleNames[0]).toHaveTextContent('Hydroponic module 1')
+  expect(moduleNames[1]).toHaveTextContent('Hydroponic module 2')
+  expect(moduleNames[2]).toHaveTextContent('Hydroponic module 3')
+})
